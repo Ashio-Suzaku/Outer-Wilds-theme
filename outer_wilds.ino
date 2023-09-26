09c0ba2ac17421759d6c1dcfbae1d49667856bf3
Ashio-Kun <120925849+Ashio-Kun@users.noreply.github.com>
2023-09-26 21:43:10 +0200

    Add files via upload

---
+++ outer_wilds.ino
@@ -0,0 +1,287 @@
+int buz = 2;
+
+void setup() {
+
+  Serial.begin(9600);
+  noTone(buz);
+
+}
+
+
+void loop() {
+
+
+  Serial.write("Motif 1 \n");
+  tone(buz, 261.63, 600); //do(bas)
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 392, 700); //sol
+  delay(700);
+
+  
+  
+  Serial.write("Motif 2 \n");
+  tone(buz, 293.66, 600); //ré
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 587.33, 350); //ré
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+
+
+  Serial.write("Motif 3 \n");
+  tone(buz, 329.63, 600); //mi
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 392, 700); //sol
+  delay(700);
+
+  
+  Serial.write("Motif 4 \n");
+  tone(buz, 293.66, 600); //ré
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 587.33, 350); //ré
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+
+
+  Serial.write("Motif 5 \n");
+  tone(buz, 261.63, 600); //do(bas)
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 392, 700); //sol
+  delay(700);
+
+
+
+
+  Serial.write("Motif 6 \n");
+  tone(buz, 261.63, 600); //do(bas)
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 587.33, 350); //ré
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+
+  
+  
+  Serial.write("Motif 7 \n");
+  tone(buz, 293.66, 600); //ré
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 392, 700); //sol
+  delay(700);
+
+
+  Serial.write("Motif 8 \n");
+  tone(buz, 329.63, 600); //mi
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 587.33, 350); //ré
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+
+  
+  Serial.write("Motif 9 \n");
+  tone(buz, 293.66, 600); //ré
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 392, 700); //sol
+  delay(700);
+
+
+  Serial.write("Motif 10 \n");
+  tone(buz, 261.63, 600); //do(bas)
+  delay(600);
+  tone(buz, 392, 600); //sol
+  delay(600);
+  tone(buz, 493.88, 1000); //si
+  delay(1000);
+
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 523.25, 350); //do(haut)
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 392, 350); //sol
+  delay(350);
+  tone(buz, 440, 350); //la
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+  tone(buz, 587.33, 350); //ré
+  delay(350);
+  tone(buz, 493.88, 350); //si
+  delay(350);
+
+}
